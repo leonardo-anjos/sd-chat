3c7324226ab3466ae334ef2c37b3fb034cc3534d
Leonardo Anjos <antonioleonardoanjos@gmail.com>
2022-09-30 17:55:04 -0300

    fix: including java classes at package

--- src/br/edu/fbuni/Client.java
+++ src/br/edu/fbuni/Client.java
@@ -1,64 +1,57 @@
+package br.edu.fbuni;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Server {
+public class Client {
     public static void main(String[] args) {
-        final ServerSocket serverSocket;
-        final Socket clientSocket;
-        final BufferedReader in;
-        final PrintWriter out;
+        final Socket clientSocket; // socket used by client to send and receive data from server
+        final BufferedReader in;   // object to read data from socket
+        final PrintWriter out;     // object to write data into socket
 
-        final Scanner sc = new Scanner(System.in);
+        final Scanner sc = new Scanner(System.in); // object to read data from user's keyboard
 
         try {
-            serverSocket = new ServerSocket(5000);
-            clientSocket = serverSocket.accept();
+            clientSocket = new Socket("127.0.0.1", 5000);
             out = new PrintWriter(clientSocket.getOutputStream());
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
             Thread sender = new Thread(new Runnable() {
                 String msg;
 
                 @Override
                 public void run() {
                     while (true) {
-                        msg = sc.nextLine();    // reads data from user's keybord
-                        out.println(msg);       // write data stored in msg in the clientSocket
-                        out.flush();            // forces the sending of the data
+                        msg = sc.nextLine();
+                        out.println(msg);
+                        out.flush();
                     }
                 }
             });
             sender.start();
-
-            Thread receive = new Thread(new Runnable() {
+            Thread receiver = new Thread(new Runnable() {
                 String msg;
 
                 @Override
                 public void run() {
                     try {
                         msg = in.readLine();
-
                         while (msg != null) {
-                            System.out.println("Client : " + msg);
+                            System.out.println("Server : " + msg);
                             msg = in.readLine();
                         }
-
-                        System.out.println("Disconnected Client");
-
+                        System.out.println("Server out of service");
                         out.close();
                         clientSocket.close();
-                        serverSocket.close();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
             });
-            receive.start();
+            receiver.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/br/edu/fbuni/Server.java
+++ src/br/edu/fbuni/Server.java
@@ -1,55 +1,66 @@
+package br.edu.fbuni;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client {
+public class Server {
     public static void main(String[] args) {
-        final Socket clientSocket; // socket used by client to send and receive data from server
-        final BufferedReader in;   // object to read data from socket
-        final PrintWriter out;     // object to write data into socket
+        final ServerSocket serverSocket;
+        final Socket clientSocket;
+        final BufferedReader in;
+        final PrintWriter out;
 
-        final Scanner sc = new Scanner(System.in); // object to read data from user's keyboard
+        final Scanner sc = new Scanner(System.in);
 
         try {
-            clientSocket = new Socket("127.0.0.1", 5000);
+            serverSocket = new ServerSocket(5000);
+            clientSocket = serverSocket.accept();
             out = new PrintWriter(clientSocket.getOutputStream());
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
             Thread sender = new Thread(new Runnable() {
                 String msg;
 
                 @Override
                 public void run() {
                     while (true) {
-                        msg = sc.nextLine();
-                        out.println(msg);
-                        out.flush();
+                        msg = sc.nextLine();    // reads data from user's keybord
+                        out.println(msg);       // write data stored in msg in the clientSocket
+                        out.flush();            // forces the sending of the data
                     }
                 }
             });
             sender.start();
-            Thread receiver = new Thread(new Runnable() {
+
+            Thread receive = new Thread(new Runnable() {
                 String msg;
 
                 @Override
                 public void run() {
                     try {
                         msg = in.readLine();
+
                         while (msg != null) {
-                            System.out.println("Server : " + msg);
+                            System.out.println("Client : " + msg);
                             msg = in.readLine();
                         }
-                        System.out.println("Server out of service");
+
+                        System.out.println("Disconnected Client");
+
                         out.close();
                         clientSocket.close();
+                        serverSocket.close();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
             });
-            receiver.start();
+            receive.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
